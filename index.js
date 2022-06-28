dd7d453ab43e93b71af53a4189e4321b9fd84417
fusion407 <fusion407@gmail.com>
2022-06-27 19:36:23 -0500

    commit tree cb50ce689e644dec69ea5084792b6834bbbe4a65

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = "Brady Dorsey";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
